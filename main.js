b4e47dc578d30f99f9b2584ec552342f153ad417
RianVitor26 <rianvitorlhe@gmail.com>
2022-08-07 01:19:39 -0300

    feat: adicionando a lógica do jump do mario

---
+++ main.js
@@ -0,0 +1,9 @@
+const mario = document.querySelector('.mario')
+
+const jump = () => {
+    mario.classList.add('jump')
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 600);
+}
+document.addEventListener("keydown", jump)
