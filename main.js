9f98c4d43794d300cc6c81a4e43f6cec68f72f00
RianVitor26 <rianvitorlhe@gmail.com>
2022-08-07 01:55:59 -0300

    feat: verificacao de colisao do mario com o pipe

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const jump = () => {
     mario.classList.add('jump')
@@ -6,4 +7,17 @@ const jump = () => {
         mario.classList.remove('jump')
     }, 600);
 }
+
+const collisionCheck = setInterval(() => {
+    const pipePosition = pipe.offsetLeft
+    const marioJumpPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ')
+
+    if (pipePosition <= 90 && marioJumpPosition < 70 && pipePosition > 0) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioJumpPosition}px`
+    }
+}, 10);
 document.addEventListener("keydown", jump)
